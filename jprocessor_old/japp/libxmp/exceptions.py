9abf56c48f444744dd336e932f94d87f3c3b73f5
fsantamaria <devnull@localhost>
2015-01-22 17:02:58 +0100

    Rimosso mini_cluster

--- jprocessor_old/japp/libxmp/exceptions.py
+++
@@ -1,3 +0,0 @@
-class XMPError(Exception):
-	""" General XMP Error. """
-	pass
--- jprocessor_old/japp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from japp.models import Job
-
-class JobSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model  = Job
-        fields = ('id', 'created', 'resource', 'data', 'duration')
