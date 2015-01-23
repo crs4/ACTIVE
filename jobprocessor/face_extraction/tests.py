3dacd834213d2943299083ccca2ccc04675e99c2
ncorona <devnull@localhost>
2015-01-23 17:33:22 +0100

    Creazione test skeleton

--- jobprocessor/face_extraction/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from face_extraction.pipe import *
-		
-class TestFunctional(TestCase):
-	
-	def test_face_extraction(self):
-		resource_path = '/var/spool/active/data/videolina-10sec.mov'
-		face_extraction(resource_path)
