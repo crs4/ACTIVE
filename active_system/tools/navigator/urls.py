19716e4b92ddd1cda254eefa7adbc34a3eda7c73
team <devnull@localhost>
2015-04-30 13:05:39 +0200

    Fix API REST docstrings
    Former-commit-id: 93169042b80ce4be8881dfb5a684c891074a9a29

--- active_system/tools/navigator/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from tools.navigator import views
-
-
-
-
-urlpatterns = [
-        url(r'^(?P<item_id>[0-9]+)/$', views.index, name="index"),
-]
