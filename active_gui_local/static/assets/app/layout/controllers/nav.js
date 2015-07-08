1b2bc3e713574595a8a8b963e76c2321b306db10
team <devnull@localhost>
2015-07-08 15:33:11 +0200

    Version reset
    Former-commit-id: cf85393b9e4ed07f39074892f51df0a35c4cf761

--- active_gui_local/static/assets/app/layout/controllers/nav.js
+++
@@ -1,9 +0,0 @@
-define(["layout/module"], function(module){
-	
-	module.registerController("navCtrl", function($scope){
-		
-		$scope.types = [{"option":"image"}, {"option":"video"}, {"option":"audio"}];
-		
-	});
-	
-});
